9b9906d06ce347b0640889f4c803a943b901d158
Your Name <you@example.com>
2020-11-12 10:07:14 -0500

    [main.css] working on styling

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 const plusButton = document.getElementById("plus")
 const minusButton = document.getElementById("minus")
-// const display = document.getElementById("display")
 display.innerText = 0
 display.value = 0
-// const input = parseInt(document.getElementById("input").value)
-// const displayValue = parseInt(document.getElementById("display").innerText)
 
 plusButton.addEventListener("click", function(){
     let input = parseInt(document.getElementById("input").value)
